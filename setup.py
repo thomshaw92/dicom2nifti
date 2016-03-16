21fd5e2e813e6879fc5314a5521d94626bdc6316
Arne Brys <arne.brys@icometrix.com>
2016-03-16 21:18:19 +0100

    DCMTONII-2: Prepare for first actual release

--- setup.py
+++ setup.py
@@ -9,13 +9,13 @@ For MR specifically there is support for most 4D data (like DTI and fMRI)
 setup(
     name='dicom2nifti',
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
-    version='0.0.1.dev2',
+    version='1.0.0.rc1',
     description='package for converting dicom files to nifti',
     long_description=long_description,
     author='icometrix',
     author_email='it@icometrix.com',
     url='https://github.com/icometrix/dicom2nifti',
-    download_url='https://github.com/icometrix/dicom2nifti/tarball/0.0.1.dev2',
+    download_url='https://github.com/icometrix/dicom2nifti/tarball/1.0.0.rc1',
     keywords=['dicom', 'nifti', 'medical imaging'],
     scripts=['scripts/dicom2nifti'],
     # https://pypi.python.org/pypi?%3Aaction=list_classifiers

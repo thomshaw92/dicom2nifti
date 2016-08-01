ba3bbfeeb39de037773980a678d7510b79ba6017
Arne Brys <arne.brys@icometrix.com>
2016-08-01 09:38:33 +0200

    DCMTONII-31: Minor release - Allow gantry tilting from command line. - Added api docs on readthedocs.org

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ For MR specifically there is support for most 4D data (like DTI and fMRI)
 setup(
     name='dicom2nifti',
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
-    version='1.1.1',
+    version='1.1.2',
     description='package for converting dicom files to nifti',
     long_description=long_description,
     license='MIT',
@@ -18,7 +18,7 @@ setup(
     maintainer = "icometrix NV",
     maintainer_email = "dicom2nifti@icometrix.com",
     url='https://github.com/icometrix/dicom2nifti',
-    download_url='https://github.com/icometrix/dicom2nifti/tarball/1.1.1',
+    download_url='https://github.com/icometrix/dicom2nifti/tarball/1.1.2',
     keywords=['dicom', 'nifti', 'medical imaging'],
     scripts=['scripts/dicom2nifti'],
     # https://pypi.python.org/pypi?%3Aaction=list_classifiers

b867051ff07def6781c0b8be7e38bb037122bdb5
Arne Brys <arne.brys@icometrix.com>
2017-04-25 11:33:40 +0200

    MSMET-929: Fix unit tests after move

--- tests/test_generic.py
+++ tests/test_generic.py
@@ -16,7 +16,7 @@ import tests.test_data as test_data
 
 import dicom2nifti.convert_generic as convert_generic
 from dicom2nifti.common import is_dicom_file, read_dicom_directory
-from test_tools import compare_nifti, ground_thruth_filenames
+from tests.test_tools import compare_nifti, ground_thruth_filenames
 
 
 class TestConversionGeneric(unittest.TestCase):

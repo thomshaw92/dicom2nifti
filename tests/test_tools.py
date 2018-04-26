d56eae1d98aad59c5bf595fa5c502f874e4a5449
Arne Brys <arne.brys@icometrix.com>
2018-04-26 13:57:48 +0200

    icometrix/dicom2nifti#34: Improved feedback during testing

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -39,6 +39,7 @@ def compare_nifti(nifti_file_1, nifti_file_2):
             result = False
         if not (nifti_1.get_data() == nifti_2.get_data()).all():
             logging.warning('data mismatch')
+            logging.warning(numpy.max(numpy.abs(nifti_1.get_data() - nifti_2.get_data())))
             result = False
 
     except:
@@ -50,10 +51,16 @@ def compare_nifti(nifti_file_1, nifti_file_2):
 def compare_bval(bval_file_1, bval_file_2):
     bval_1 = numpy.loadtxt(bval_file_1)
     bval_2 = numpy.loadtxt(bval_file_2)
-    return numpy.array_equal(bval_1, bval_2)
+    equal = numpy.array_equal(bval_1, bval_2)
+    if not equal:
+        logging.warning('bvals not equal\n%s\n', bval_1, bval_2)
+    return equal
 
 
 def compare_bvec(bvec_file_1, bvec_file_2):
     bvec_1 = numpy.loadtxt(bvec_file_1)
     bvec_2 = numpy.loadtxt(bvec_file_2)
-    return numpy.array_equal(bvec_1, bvec_2)
+    equal = numpy.array_equal(bvec_1, bvec_2)
+    if not equal:
+        logging.warning('bvecs not equal\n%s\n', bvec_1, bvec_2)
+    return equal

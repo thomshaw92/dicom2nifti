f9f050593dba592fbc12875c527cac38173b7c8b
Arne Brys <arne.brys@icometrix.com>
2018-04-26 14:09:56 +0200

    icometrix/dicom2nifti#34: Allow for rounding differences

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -37,23 +37,8 @@ def compare_nifti(nifti_file_1, nifti_file_2):
         if nifti_1.get_data_dtype() != nifti_2.get_data_dtype():
             logging.warning('dtype mismatch')
             result = False
-        if not (nifti_1.get_data() == nifti_2.get_data()).all():
+        if not numpy.allclose(nifti_1.get_data(), nifti_2.get_data()):
             logging.warning('data mismatch')
-            logging.warning('perc 10:\n%s\n%s',
-                            numpy.percentile(nifti_1.get_data(), 10),
-                            numpy.percentile(nifti_2.get_data(), 10))
-            logging.warning('perc 25:\n%s\n%s',
-                            numpy.percentile(nifti_1.get_data(), 25),
-                            numpy.percentile(nifti_2.get_data(), 25))
-            logging.warning('perc 50:\n%s\n%s',
-                            numpy.percentile(nifti_1.get_data(), 50),
-                            numpy.percentile(nifti_2.get_data(), 50))
-            logging.warning('perc 75:\n%s\n%s',
-                            numpy.percentile(nifti_1.get_data(), 75),
-                            numpy.percentile(nifti_2.get_data(), 75))
-            logging.warning('perc 90:\n%s\n%s',
-                            numpy.percentile(nifti_1.get_data(), 90),
-                            numpy.percentile(nifti_2.get_data(), 90))
             result = False
 
     except:
@@ -65,7 +50,7 @@ def compare_nifti(nifti_file_1, nifti_file_2):
 def compare_bval(bval_file_1, bval_file_2):
     bval_1 = numpy.loadtxt(bval_file_1)
     bval_2 = numpy.loadtxt(bval_file_2)
-    equal = numpy.array_equal(bval_1, bval_2)
+    equal = numpy.allclose(bval_1, bval_2)
     if not equal:
         logging.warning('bvals not equal\n%s\n', numpy.array2string(bval_1), numpy.array2string(bval_2))
     return equal
@@ -74,7 +59,7 @@ def compare_bval(bval_file_1, bval_file_2):
 def compare_bvec(bvec_file_1, bvec_file_2):
     bvec_1 = numpy.loadtxt(bvec_file_1)
     bvec_2 = numpy.loadtxt(bvec_file_2)
-    equal = numpy.array_equal(bvec_1, bvec_2)
+    equal = numpy.allclose(bvec_1, bvec_2)
     if not equal:
         logging.warning('bvecs not equal\n%s\n', numpy.array2string(bvec_1), numpy.array2string(bvec_2))
     return equal
